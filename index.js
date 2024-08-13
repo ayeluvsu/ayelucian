a6e4e66e461022a876f6861bb1c830a476b560f0
ayeluvsu <158154505+ayeluvsu@users.noreply.github.com>
2024-08-14 02:29:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = [".gg/angelshp","Browsing Angel Shop","Verified Resellers"];
 
 
 let currentIndex = 0;
